a9145496db66efeb0cf7d1b1086d89e3ebb8c91f
linko1994 <Vorbild94@gmail.com>
2018-04-30 14:44:15 +0200

    added javascript scroll anchor

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function scrolling() {
+  var elemnt = document.getElementById('anchor');
+  elemnt.scrollIntoView();
+}
+
+function scrolling2() {
+  var elemnt2 = document.getElementById("anchor2");
+  elemnt2.scrollIntoView();
+}
